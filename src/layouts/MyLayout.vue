3beff7520043782ff7010ac4bbe55f1149516b23
lucas.camacho <lucas.camacho@animaeducacao.com.br>
2019-02-11 11:25:32 -0200

    First commit

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,70 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-layout-header>
+      <q-toolbar
+        color="negative"
+        :glossy="$q.theme === 'mat'"
+        :inverted="$q.theme === 'ios'"
+      >
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+
+        <q-toolbar-title>
+          Quasar App Test Ciandt
+          <div slot="subtitle">Running on Quasar v{{ $q.version }}</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header>
+
+    <q-layout-drawer
+      v-model="leftDrawerOpen"
+      :content-class="$q.theme === 'mat' ? 'bg-grey-2' : null"
+    >
+      <q-list
+        no-border
+        link 
+        inset-delimiter
+      >
+        <q-list-header>Menu</q-list-header>
+        <q-item to="/">
+          <q-item-side icon="home" />
+          <q-item-main label="Dashboard" />
+        </q-item>
+        <q-item to="/posts">
+          <q-item-side icon="list_alt" />
+          <q-item-main label="Postagens" sublabel="Lista de postagem" />
+        </q-item>
+      </q-list>
+    </q-layout-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/posts/posts.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="container q-pa-xs">
+    <div class="row gutter-sm">
+        <div class="col-xs-12 col-lg-4" v-for="artigo in 10" :key="artigo">
+            <q-card>
+                <q-card-media>
+                    <img src="~assets/logo.png" style="width: 30rem">
+                </q-card-media>
+                <q-card-title>
+                    Título do Artigo
+                </q-card-title>
+                <q-card-separator/>
+                <q-card-main>
+                    Resumo do Artigo ...
+                </q-card-main>
+                <q-card-actions>
+                    <q-btn push color="dark" label="Detalhes" class="full-width" />
+                </q-card-actions> 
+            </q-card>
+        </div>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    name: 'Posts'
+})
+</script>
+
+<style>
+ 
+</style>
+
+
